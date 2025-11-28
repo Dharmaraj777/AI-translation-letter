fe30921b69984250439ddc6ab7525b2f15008835
Dharma raja B L <87357702+Dharmaraj777@users.noreply.github.com>
2025-11-28 19:23:28 +0530

    Create __init__.py

---
+++ translators/__init__.py
@@ -0,0 +1,5 @@
+from .docx_translator import DocxTranslator
+from .pptx_translator import PptxTranslator
+from .pdf_translator import PdfTranslator
+
+__all__ = ["DocxTranslator", "PptxTranslator", "PdfTranslator"]
