53ac24e1378e396b99e54cf2cfe056972d4b792a
Dharma raja B L <87357702+Dharmaraj777@users.noreply.github.com>
2025-12-01 22:11:50 +0530

    Update __init__.py

--- translators/__init__.py
+++ translators/__init__.py
@@ -1,5 +1,22 @@
+# translators/__init__.py
+
+from ai_translation_oai_client import OaiClient
+
 from .docx_translator import DocxTranslator
 from .pptx_translator import PptxTranslator
 from .pdf_translator import PdfTranslator
 
-__all__ = ["DocxTranslator", "PptxTranslator", "PdfTranslator"]
+
+def get_translators(oai_client: OaiClient):
+    """
+    Factory to create all translator instances for a given OaiClient.
+
+    Each translator implements:
+      - can_handle(filename: str) -> bool
+      - translate_document(filename, content_bytes, target_language, target_dialect) -> bytes
+    """
+    return [
+        DocxTranslator(oai_client),
+        PptxTranslator(oai_client),
+        PdfTranslator(oai_client),
+    ]
